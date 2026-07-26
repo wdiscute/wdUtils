7185ccc817d7577e907ac7081b16cedc00f51c9a
wd <lucasalvesbispo2@gmail.com>
2026-07-26 19:27:04 +0100

    added TickableBlockEntity

---
+++ src/main/java/com/wdiscute/utils/TickableBlockEntity.java
@@ -0,0 +1,45 @@
+package com.wdiscute.utils;
+
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.state.BlockState;
+
+public interface TickableBlockEntity
+{
+    default void tickServer(ServerLevel level, BlockPos pos, BlockState state)
+    {
+        tick(level, pos, state);
+    }
+
+    default void tickClient(ClientLevel level, BlockPos pos, BlockState state)
+    {
+        tick(level, pos, state);
+    }
+
+    default void tick(Level level, BlockPos pos, BlockState state)
+    {
+
+    }
+
+    static <T extends BlockEntity> BlockEntityTicker<T> getTicketHelper(Level level)
+    {
+        if (level.isClientSide())
+        {
+            return (l, bp, bs, be) ->
+            {
+                if (be instanceof TickableBlockEntity tbe) tbe.tickClient((ClientLevel) level, bp, bs);
+            };
+        }
+        else
+        {
+            return (l, bp, bs, be) ->
+            {
+                if (be instanceof TickableBlockEntity tbe) tbe.tickServer((ServerLevel) level, bp, bs);
+            };
+        }
+    }
+}
